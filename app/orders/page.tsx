95f8ccccd34214c84907400f9cd00b3f9b6a4a34
Alp Sahin <alp@sahin.club>
2024-12-26 01:23:26 +0300

    Renamed orders-page.tsx file

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,6 +1,6 @@
 import '@/app/ui/global.css'
 import { Suspense } from "react";
-import OrdersPage from "@/app/lib/orders_page";
+import OrdersPage from "@/app/lib/orders-page";
 
 export default async function Home(props: { searchParams?: Promise<{ buy: string; sell: string; page: number }> }) {
   return (
